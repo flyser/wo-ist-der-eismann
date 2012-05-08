921185a9763c323ec42b96daf586783a1369c670
Fabian Henze <flyser42@gmx.de>
2012-05-08 20:03:45 +0200

    Update packageplasmoid.sh to create .plasmoid files instead of .zip files
    and update .gitignore

--- packageplasmoid.sh
+++ packageplasmoid.sh
@@ -3,11 +3,15 @@
 set -e
 cd "$(dirname "$0")"
 
-if [ -e wo-ist-der-eismann.zip ] ; then
-  rm -i wo-ist-der-eismann.zip
-  [ -e wo-ist-der-eismann.zip ] && exit 1
-fi
-zip -r wo-ist-der-eismann.zip . -x '*~' '*.zip' '.git*' updatejsfiles.sh packageplasmoid.sh runplasmoidviewer.sh
+for file in wo-ist-der-eismann.plasmoid ; do
+  if [ "$1" == "-f" ]; then
+    rm -f "$file"
+  else
+    [ -e "$file" ] && rm -i "$file"
+  fi
+  [ -e "$file" ] && exit 1
+done
+zip -r wo-ist-der-eismann.plasmoid . -x '*~' '*.zip' '.git*' updatejsfiles.sh packageplasmoid.sh runplasmoidviewer.sh
 
 echo "Did you remember, to:"
 echo " * Update the version in the .desktop file"
